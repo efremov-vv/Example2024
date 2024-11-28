c22b59d309b26a8e0dbd7e60913e09fdf0864955
V.Efremov <vve.swsu@yandex.ru>
2024-11-28 18:31:04 +0300

    First example

--- comph.py
+++ comph.py
@@ -5,6 +5,4 @@ str1 = [str(i) + '^2=' + str(i ** 2) for i in range(1, 10)]
 print('\n'.join(str1))
 
 
-#print(' '.join(input().split()[2::3]))
-
 print('\n'.join(str(i ** 2) for i in range(int(input()))))
