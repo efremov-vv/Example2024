c555e43b1b4155b8c78a0913339a74850e8a2e83
V.Efremov <vve.swsu@yandex.ru>
2024-11-28 18:25:58 +0300

    First change

--- comph.py
+++ comph.py
@@ -1,10 +1,5 @@
 print([i / j for i in range(1, 4) for j in range(4, 7)])
-
-
 str1 = [str(i) + '^2=' + str(i ** 2) for i in range(1, 10)]
 print('\n'.join(str1))
-
-
-#print(' '.join(input().split()[2::3]))
-
+print(' '.join(input().split()[2::3]))
 print('\n'.join(str(i ** 2) for i in range(int(input()))))
