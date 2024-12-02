b39befc161765b512e15b3aeef23c1b1dd9153a1
V.Efremov <vve.swsu@yandex.ru>
2024-12-02 17:56:18 +0300

    demobranch коммит

--- comph.py
+++ comph.py
@@ -1,10 +1,11 @@
-"""print()
+print()
 print([i / j for i in range(1, 4) for j in range(4, 7)])
 str1 = [str(i) + '^2=' + str(i ** 2) for i in range(1, 10)]
 print('\n'.join(str1))
 print(' '.join(input().split()[2::3]))
 print('\n'.join(str(i ** 2) for i in range(int(input()))))
-print()"""
+print()
+
 print(' '.join([str(int(i) ** 2) for i in input().split() if int(i) % 2 != 0 and i[-1] != '9']))
 
 name = "Аркадий"
